6305cd84c463ee514f55c1054f483ae4710d24cd
minuth <33181465+minuth@users.noreply.github.com>
2020-05-29 15:12:39 +0700

    Update Main.java

--- src/com/minuth/hackerrank/codechallenges/Main.java
+++ src/com/minuth/hackerrank/codechallenges/Main.java
@@ -4,7 +4,38 @@ package com.minuth.hackerrank.codechallenges;
 public class Main {
 
     public static void main(String[] args) {
-        MyRegex.printRegex("[a-zA-Z][\\w]{7,29}","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        //MyRegex.printRegex("[a-zA-Z][\\w]{7,29}","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        try {
+            foo("16 00 00");
+            foo("16 1 16");
+            foo("16 2 20");
+            foo("18 33 9");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    static void foo(String value) throws Exception {
+        String []rawData = value.split(" ");
+        if(rawData.length < 3){
+            throw new Exception("Invalid data!!!");
+        }
+        int h = Integer.parseInt(rawData[0]);
+        int m = Integer.parseInt(rawData[1]);
+        int s = Integer.parseInt(rawData[2]);
+
+        if(!(h >= 16 && h <= 17) || (h >= 17 && (m > 0 || s > 0))){
+            System.out.println("No information");
+        }
+        else {
+
+            if((m % 2 == 0 && s > 15)||(m % 2 != 0 && s < 15)){
+                System.out.println("working out");
+            }
+            else {
+                System.out.println("resting");
+            }
+        }
     }
 
 }
